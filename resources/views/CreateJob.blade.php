12fd99435751eb531ca28641113fd1d952cffa85
hammastahir11 <hammastahir11@gmail.com>
2022-06-09 19:54:39 +0500

    hammas changing2

--- resources/views/CreateJob.blade.php
+++ resources/views/CreateJob.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{URL::asset('public/css/CreateJob/CreateJob.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('css/CreateJob/CreateJob.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
@@ -17,7 +17,7 @@
     <title>Job Create</title>
 </head>
 <body class="bgcolorp">
-    <div class="container text-center py-3 text-light">
+    <div class="container text-center py-3 text-dark">
         <h3>Find a Great Hire, Fast</h3>
         <p>Rated #1 in delivering quality hires</p>
         <div class="d-flex justify-content-center  p-3  " >
@@ -67,10 +67,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="ckeditor_4.19.0_standard/ckeditor/ckeditor.js"></script>
-    <script>
+    <!-- <script>
         //Editor for a discription in the job
         CKEDITOR.replace('content');
-    </script>
+    </script> -->
 </body>
 </html>
 @endsection
