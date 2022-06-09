956ff826375da975e48389cb2d6309643432fd19
kashirao1122 <www.kashirao147.com@gmail.com>
2022-06-09 23:22:43 +0500

    kashif jobApply

--- resources/views/CreateJob.blade.php
+++ resources/views/CreateJob.blade.php
@@ -1,25 +1,29 @@
-<!DOCTYPE html>
 @extends('Layouts.layout')
 @section('content')
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{URL::asset('css/CreateJob/CreateJob.css')}}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-      crossorigin="anonymous"
-    />
-    <title>Job Create</title>
-</head>
-<body class="bgcolorp">
-    <div class="container text-center py-3 text-dark">
-        <h3>Find a Great Hire, Fast</h3>
-        <p>Rated #1 in delivering quality hires</p>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{URL::asset('css/CreateJob/CreateJob.css')}}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+        <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        crossorigin="anonymous"
+        />
+        <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script>
+        <title>Job Create</title>
+    </head>
+    <body >
+    <div class="bgcolorp">
+        <div class="container  text-center py-3 text-light">
+            <div class="parrotColor">Lannat Be Shamar</div>
+            <h3>Find a Great Hire, Fast</h3>
+            
+            <p>Rated #1 in delivering quality hires</p>
         <div class="d-flex justify-content-center  p-3  " >
             <form class=" border rounded w-50 p-2 text-start" action="">
                 <label for="title" class="py-2">Job Title</label><br>
@@ -46,14 +50,16 @@
                 </select>
 
                 <label for="description" class="py-2">Description</label><br>
-                <textarea name="content"  id="description" class="w-100"></textarea>
+                <textarea name="editor"  id="editor" rows="10" class="w-100"></textarea>
 
                 <button class="rounded border w-100 py-2 my-2 btn-primary">Post</button>
 
 
             </form>
         </div>
+        </div>
     </div>
+
 
 
 
@@ -66,12 +72,15 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="ckeditor_4.19.0_standard/ckeditor/ckeditor.js"></script>
-    <script>
-        //Editor for a discription in the job
 
-        CKEDITOR.replace('content');
+    <script>
+                            ClassicEditor.create( document.querySelector( '#editor' ) ).then( editor => {
+                                            console.log( editor );
+                                    } ).catch( error => {
+                                            console.error( error );
+                                    } );
     </script>
+
 
 </body>
 </html>
