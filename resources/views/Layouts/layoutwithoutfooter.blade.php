9509922ca1c1e88cc008e66bfa49c3e0a07f1f5e
hammastahir11 <hammastahir11@gmail.com>
2022-07-02 18:08:59 +0500

    icons12

--- resources/views/Layouts/layoutwithoutfooter.blade.php
+++ resources/views/Layouts/layoutwithoutfooter.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="{{URL::asset('css/layout/menu.css')}}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
@@ -52,10 +52,14 @@
           <div class="dd_menu">
 
             <div class="dd_left">
-              <ul>
-                <li><i class="fas fa-cog"></i></li>
-                <li><i class="fas fa-download"></i></li>
-                <li><i class="fas fa-sign-out-alt"></i></li>
+            <ul>
+                <li><i class="bi bi-arrow-right-square-fill"></i></i></li>
+                <li><i class="bi bi-view-stacked"></i></li>
+                <li><i class="bi bi-archive"></i></li>
+                <li><i class="bi bi-journals"></i></li>
+                <li><i class="bi bi-book-half"></i></li>
+                <br/>
+                <li><i class="bi bi-box-arrow-in-right"></i></li>
               </ul>
             </div>
             <div class="dd_right">
